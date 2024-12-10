37339cb71456ee6efd6893a604639a89a4a21b4a
Gregor Gabrovšek <gregor.gabrovsek@sunesis.si>
2024-12-10 09:06:17 +0100

    Solved 2024d10

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,5 +1,13 @@
 import time
 
+def find_zero_indices(grid: list[list[int]]) -> set[tuple[int, int]]:
+    results = set()
+    for inX, x in enumerate(grid):
+        for inY, y in enumerate(x):
+            if y == 0:
+                results.add((inX, inY))
+    return results
+
 
 def task1():
     with open("d10input.txt", "r") as f:
@@ -7,14 +15,6 @@ def task1():
     ttt = time.time()
 
     grid = [[int(x) for x in y] for y in lines]
-
-    def find_zero_indices(grid: list[list[int]]) -> set[tuple[int, int]]:
-        results = set()
-        for inX, x in enumerate(grid):
-            for inY, y in enumerate(x):
-                if y == 0:
-                    results.add((inX, inY))
-        return results
 
     def generate_neighbors_from_positions(positions: set[tuple[int, int]], grid_size_x: int, grid_size_y: int) -> set[
         tuple[int, int]]:
@@ -60,6 +60,43 @@ def task2():
     with open("d10input.txt", "r") as f:
         lines = f.read().split("\n")
     ttt = time.time()
+
+    grid = [[int(x) for x in y] for y in lines]
+
+    def recurse(grid: list[list[int]], x: int, y: int, expected_value: int) -> int:
+        if grid[x][y] == "x":
+            return 0
+
+        if grid[x][y] != expected_value:
+            return 0
+
+        if grid[x][y] == 9:
+            return 1
+
+        print(f"Found {expected_value} at ")
+
+        grid_copy = [[xx for xx in yy] for yy in grid]
+        grid_copy[x][y] = "x"
+
+        locations = [
+            (x, y+1),
+            (x, y-1),
+            (x+1, y),
+            (x-1, y),
+        ]
+
+
+        return sum(
+            recurse(grid_copy, loc[0], loc[1], expected_value + 1)
+            for loc in locations
+            if 0 <= loc[0] < len(grid) and 0 <= loc[1] < len(grid[0])
+        )
+
+    zeroes = find_zero_indices(grid)
+    paths = 0
+    for zeroX, zeroY in zeroes:
+        paths += recurse(grid, zeroX, zeroY, 0)
+    print(paths)
 
     print(time.time() - ttt)
 
